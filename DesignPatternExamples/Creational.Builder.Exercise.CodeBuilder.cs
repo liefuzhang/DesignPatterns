359f0c9c8acce72d806c475f1136309c400c8572
liefu zhang <liefuzhangnz@gmail.com>
2018-05-27 22:48:02 +1200

    Builder and Factory

---
+++ DesignPatternExamples/Creational.Builder.Exercise.CodeBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coding.Exercise {
+    public class ClassObj {
+        public string Name;
+        public int IndentSize = 2;
+        public List<Tuple<string, string>> Attrs = new List<Tuple<string, string>>();
+        
+        public override string ToString() {
+            var sb = new StringBuilder();
+            sb.AppendLine($"public class {Name}");
+            sb.AppendLine("{");
+            foreach (var attr in Attrs) {
+                var i = new string(' ', IndentSize);
+                sb.AppendLine($"{i}public {attr.Item2} {attr.Item1};");
+            }
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+    }
+
+    public class CodeBuilder {
+        private ClassObj classObj = new ClassObj();
+        private readonly string className;
+
+        public CodeBuilder(string name) {
+            className = name;
+            classObj.Name = name;
+        }
+
+        public CodeBuilder AddField(string name, string type) {
+            classObj.Attrs.Add(new Tuple<string, string>(name, type));
+            return this;
+        }
+
+        public override string ToString() {
+            return classObj.ToString();
+        }
+
+        public void Clear() {
+            classObj = new ClassObj {Name = className};
+        }
+    }
+
+    public class Demo {
+        public static void Main(string[] args) {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+        }
+    }
+}
---
+++ DesignPatternExamples/Creational.Factories.Factory.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace DotNetDesignPatternDemos.Creational.Factories {
+    public class Point {
+        private double x, y;
+
+        protected Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(double a,
+          double b, // names do not communicate intent
+          CoordinateSystem cs = CoordinateSystem.Cartesian) {
+            switch (cs) {
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = a * Math.Sin(b);
+                    break;
+                default:
+                    x = a;
+                    y = b;
+                    break;
+            }
+
+            // steps to add a new system
+            // 1. augment CoordinateSystem
+            // 2. change ctor
+        }
+
+        // factory property
+
+        public static Point Origin => new Point(0, 0);
+
+        // singleton field
+        public static Point Origin2 = new Point(0, 0);
+
+        // factory method
+
+        public static Point NewCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        public enum CoordinateSystem {
+            Cartesian,
+            Polar
+        }
+
+        // make it lazy
+        public static class Factory {
+            public static Point NewCartesianPoint(double x, double y) {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta) {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+    }
+
+    class PointFactory {
+        public static Point NewCartesianPoint(float x, float y) {
+            return new Point(x, y); // needs to be public
+        }
+    }
+
+    class Demo {
+        static void Main(string[] args) {
+            var p1 = new Point(2, 3, Point.CoordinateSystem.Cartesian);
+            var origin = Point.Origin;
+
+            var p2 = Point.Factory.NewCartesianPoint(1, 2);
+            var p3 = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
+        }
+    }
+}
