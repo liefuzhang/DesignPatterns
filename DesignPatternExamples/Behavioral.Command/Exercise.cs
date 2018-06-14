5e2d8126223809dd41d532e0cee7c3286dfbe3da
liefu <liefu@flowingly.net>
2018-06-14 15:16:01 +1200

    command

---
+++ DesignPatternExamples/Behavioral.Command/Exercise.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Coding.Exercise
+{
+    public class Command
+    {
+        public enum Action
+        {
+            Deposit,
+            Withdraw
+        }
+
+        public Action TheAction;
+        public int Amount;
+        public bool Success;
+    }
+
+    public class Account
+    {
+        public int Balance { get; set; }
+
+        public void Process(Command c)
+        {
+            // todo
+            c.Success = false;
+            switch (c.TheAction)
+            {
+                case Command.Action.Deposit:
+                    Balance += c.Amount;
+                    c.Success = true;
+                    break;
+                case Command.Action.Withdraw:
+                    if (Balance >= c.Amount)
+                    {
+                        Balance -= c.Amount;
+                        c.Success = true;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
