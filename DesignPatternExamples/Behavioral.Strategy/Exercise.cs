2804bc44e4241637f88a737294f69eda5a00615f
liefu zhang <liefuzhangnz@gmail.com>
2018-06-21 21:57:56 +1200

    Template method

---
+++ DesignPatternExamples/Behavioral.Strategy/Exercise.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Numerics;
+using NUnit.Framework;
+
+namespace Coding.Exercise {
+    public interface IDiscriminantStrategy {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy {
+        // todo
+        public double CalculateDiscriminant(double a, double b, double c) {
+            return b * b - 4 * a * c;
+        }
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy {
+        // todo (return NaN on negative discriminant!)
+
+        public double CalculateDiscriminant(double a, double b, double c) {
+            return b * b < 4 * a * c ? Double.NaN : b * b - 4 * a * c;
+        }
+    }
+
+    public class QuadraticEquationSolver {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy) {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c) {
+            // todo
+            var discriminant = strategy.CalculateDiscriminant(a, b, c);
+            if (double.IsNaN(discriminant)) {
+                return new Tuple<Complex, Complex>(new Complex(Double.NaN, Double.NaN), new Complex(Double.NaN, Double.NaN));
+            }
+
+            if (discriminant < 0) {
+                double imaginary = Math.Sqrt(-discriminant);
+                return new Tuple<Complex, Complex>(new Complex(-b / 2 * a, imaginary / 2 * a), new Complex(-b / 2 * a, -imaginary / 2 * a));
+            }
+
+            return new Tuple<Complex, Complex>((-b + Math.Sqrt(discriminant)) / 2 * a, (-b - Math.Sqrt(discriminant)) / 2 * a);
+        }
+
+        public static void Main(string[] args) {
+
+        }
+    }
+
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void PositiveTestOrdinaryStrategy() {
+            var strategy = new OrdinaryDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 10, 16);
+            Assert.That(results.Item1, Is.EqualTo(new Complex(-2, 0)));
+            Assert.That(results.Item2, Is.EqualTo(new Complex(-8, 0)));
+        }
+
+        [Test]
+        public void PositiveTestRealStrategy() {
+            var strategy = new RealDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 10, 16);
+            Assert.That(results.Item1, Is.EqualTo(new Complex(-2, 0)));
+            Assert.That(results.Item2, Is.EqualTo(new Complex(-8, 0)));
+        }
+
+        [Test]
+        public void NegativeTestOrdinaryStrategy() {
+            var strategy = new OrdinaryDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 4, 5);
+            Assert.That(results.Item1, Is.EqualTo(new Complex(-2, 1)));
+            Assert.That(results.Item2, Is.EqualTo(new Complex(-2, -1)));
+        }
+
+        [Test]
+        public void NegativeTestRealStrategy() {
+            var strategy = new RealDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 4, 5);
+            var complexNaN = new Complex(double.NaN, double.NaN);
+
+            Assert.That(results.Item1, Is.EqualTo(complexNaN));
+            Assert.That(results.Item2, Is.EqualTo(complexNaN));
+
+            Assert.IsTrue(double.IsNaN(results.Item1.Real));
+            Assert.IsTrue(double.IsNaN(results.Item1.Imaginary));
+            Assert.IsTrue(double.IsNaN(results.Item2.Real));
+            Assert.IsTrue(double.IsNaN(results.Item2.Imaginary));
+        }
+    }
+}
